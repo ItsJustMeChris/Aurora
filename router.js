64bbcd6e22727353b0f058bdbca457ce3e0bf67d
Chris <dribbleribbletribble@gmail.com>
2018-10-15 02:21:19 -0400

    Update router.js

--- router.js
+++ router.js
@@ -1,16 +1,18 @@
-let router = require('aurora-router');
+    let router = require('aurora-router');
 
-router.route('/', 'get', (req, res) => {
-    res.write("From Floof Route");
-    res.end();
-});
+    router.route('/', 'get', (req, res) => {
+        res.write("From index route");
+        res.end();
+    });
 
-router.route('/user/:id', 'get', (req, res) => {
-    res.write("From Floof Route With Params: ");
-    res.end();
-});
+    router.route('/user/:id', 'get', (req, res) => {
+        res.write("From user/:id");
+        res.end();
+    });
 
-router.route('/user/panel', 'get', (req, res) => {
-    res.write("user/panel");
-    res.end();
-});
+    router.route('/user/panel', 'get', (req, res) => {
+        res.write("user/panel");
+        res.end();
+    });
+
+    router.route('/user/update', 'post', 'User#update')
